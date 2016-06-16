e712328c2fb10df8cab3498ac782e1a2f2aaffea
Krystian Ryniec <187796@edu.p.lodz.pl>
2016-06-16 17:07:32 +0200

    refactorization

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -4,6 +4,7 @@ package edu.iis.mto.serverloadbalancer;
  * Created by student on 2016-06-16.
  */
 public class Server {
+    public static final double MAXIMUM_LOAD = 100.0d;
     public double currentLoadOf;
     public int capacity;
 
@@ -11,5 +12,9 @@ public class Server {
 
         this.capacity = capacity;
 
+    }
+
+    public void addVm(Vm[] vm) {
+        currentLoadOf = (double) vm[0].size / (double) capacity * MAXIMUM_LOAD;
     }
 }
